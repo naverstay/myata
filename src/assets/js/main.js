352eaf006b2347545b0248bc6e0b0838c34c9e30
nv <naverstay.me@gmail.com>
2023-11-20 22:11:05 +0100

    burger

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,94 @@
-console.log("Hello world!");
+import {debounce} from "throttle-debounce";
+
+let openMobileMenu = false;
+let prevScrollPos = 0;
+
+const toggleMobileMenu = (e) => {
+  e?.preventDefault();
+  openMobileMenu = !openMobileMenu;
+  document.documentElement.classList.toggle("__open-mob-menu", openMobileMenu);
+  return false;
+};
+
+const initBurger = () => {
+  document.querySelectorAll(".js-open-menu").forEach((item) => {
+    item.addEventListener("click", toggleMobileMenu);
+  });
+};
+
+const getScrollbarWidth = () => {
+  // Creating invisible container
+  const outer = document.createElement("div");
+  outer.style.visibility = "hidden";
+  outer.style.overflow = "scroll"; // forcing scrollbar to appear
+  outer.style.msOverflowStyle = "scrollbar"; // needed for WinJS apps
+  document.body.appendChild(outer);
+
+  // Creating inner element and placing it in the container
+  const inner = document.createElement("div");
+  outer.appendChild(inner);
+
+  // Calculating difference between container's full width and the child width
+  const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
+
+  // Removing temporary elements from the DOM
+  outer.parentNode.removeChild(outer);
+
+  return scrollbarWidth;
+};
+
+function getScrollTop() {
+  return (
+    window.pageYOffset ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop ||
+    0
+  );
+}
+
+const appHeight = () => {
+  const doc = document.documentElement;
+  const sab =
+    parseInt(
+      getComputedStyle(document.documentElement).getPropertyValue("--sab")
+    ) || 0;
+  doc.style.setProperty(
+    "--app-height",
+    `${Math.max(300, window.innerHeight - 1 - sab)}px`
+  );
+  doc.style.setProperty("--app-scroll-size", getScrollbarWidth() + "px");
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  appHeight();
+  initBurger();
+});
+
+const debounceScrollUpCheck = debounce(20, (newScrollTop) => {
+  document.documentElement.classList.toggle(
+    "__scroll-hide",
+    newScrollTop > 100
+  );
+  document.documentElement.classList.toggle(
+    "__scroll-up",
+    newScrollTop === 0 ? false : prevScrollPos > newScrollTop
+  );
+  prevScrollPos = newScrollTop;
+});
+
+
+function checkWindowScroll() {
+  const newScrollTop = getScrollTop();
+  document.documentElement.classList.toggle("__scrolled", newScrollTop > 0);
+  debounceScrollUpCheck(newScrollTop);
+}
+
+checkWindowScroll();
+
+window.addEventListener("resize", () => {
+  appHeight();
+});
+
+window.addEventListener("scroll", (e) => {
+  checkWindowScroll();
+});
